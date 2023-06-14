7923ca0469f0ff582a6163e5137834f98f2a1d81
Kevin Yang <therealkevinyang@gmail.com>
2023-06-14 16:06:32 -0400

    [C2] Updated scatter plots

--- server.py
+++ server.py
@@ -9,29 +9,41 @@ TEMPLATE_PATH = "./templates"
 
 app = Flask(__name__)
 
-questions = [
+graphSet = [
     {
         "id": 1,
-        "question": "Question 1",
-        "options": ["Option 1", "Option 2", "Option 3", "Option 4"],
         "graphImage": "../static/assets/graphs/graph1.png",
-        "correctAnswer": 0,
-    },
-    {
-        "id": 2,
-        "question": "Question 2",
-        "options": ["Option 10", "Option 2", "Option 3", "Option 4"],
-        "graphImage": "../static/assets/graphs/graph2.png",
-        "correctAnswer": 1,
-    },
-    {
-        "id": 3,
-        "question": "Question 3",
-        "options": ["Option 10", "Option 2", "Option 3", "Option 4"],
-        "graphImage": "../static/assets/graphs/graph3.png",
-        "correctAnswer": 1,
+        "questions": [
+            {
+                "id": 1,
+                "question": "Question 1",
+                "options": ["Option 1", "Option 2", "Option 3", "Option 4"],
+                "correctAnswer": 0,
+            },
+            {
+                "id": 2,
+                "question": "Question 2",
+                "options": ["Option 10", "Option 2", "Option 3", "Option 4"],
+                "correctAnswer": 1,
+            },
+            {
+                "id": 3,
+                "question": "Question 3",
+                "options": ["Option 10", "Option 2", "Option 3", "Option 4"],
+                "correctAnswer": 1,
+            },
+        ],
     },
 ]
+
+def getGraphIds():
+    ids = [ele["id"] for ele in graphSet]
+    return ids
+
+
+def getRandomGraph():
+    randomNumber = random.randint(0, len(graphSet) - 1)
+    return graphSet[randomNumber]
 
 
 def getNumberOfFiles(path):
@@ -41,70 +53,79 @@ def getNumberOfFiles(path):
 def getFileName(path, index):
     return path + "/" + os.listdir(TEMPLATE_PATH + path)[index]
 
-
 @app.route("/")
 def home():
     fileName = "/index.html"
-    resp = make_response(render_template(fileName))
+    resp = make_response(render_template(fileName, graphIds=getGraphIds()))
     return resp
-
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-
 @app.route("/infographics")
 def info_graphics():
-    resp = make_response(render_template("infographics.html", questions=questions))
+    resp = make_response(
+        render_template("infographics.html", graphSet=getRandomGraph())
+    )
     resp.set_cookie("track_time", datetime.datetime.now().isoformat())
     return resp
 
 
-def getQuestion(id):
-    for question in questions:
+def getGraphSet(graphId):
+    for ele in graphSet:
+        if ele["id"] == graphId:
+            return ele
+    return None
+
+
+def getQuestion(graphId, id):
+    graphSet = getGraphSet(graphId)
+    for question in graphSet["questions"]:
         if question["id"] == id:
             return question
     return None
 
 
-def getScore(data):
+def getScore(graphId, data):
     score = 0
     for ele in data:
         # find the question with id
-        question = getQuestion(ele["id"])
+        question = getQuestion(graphId, ele["id"])
         if question["correctAnswer"] == ele["answer"]:
             score += 1
 
-    return (score/len(data))
+    return score / len(data)
 
 
 @app.route("/submit", methods=["POST"])
 def submit():
     data = request.get_json()
-    # print(request.cookies)
-    # time_str = request.cookies["track_time"]
-    # print(time_str)
-    # old_time = datetime.datetime.fromisoformat(time_str)
-    # curr_time = datetime.datetime.now()
-    # time = (curr_time - old_time).seconds
-    d = {"time": data["totalTime"], "score": getScore(data["answers"])}
-    print(d)
+    d = {
+        "graphId": data["graphId"],
+        "time": data["totalTime"],
+        "score": getScore(data["graphId"], data["answers"]),
+    }
     write_to_csv(d)
     return json.dumps({"success": True}), 200, {"ContentType": "application/json"}
 
 
 @app.route("/analysis")
 def analysis():
-    X, Y = read_csv("./data.csv")
+    graphId = int(request.args.get("graphId"))
+    X, Y = read_csv("./data.csv", graphId)
+
     fileName = "/analysis.html"
-    print(fileName)
-    resp = make_response(render_template(fileName, X=X, Y=Y))
+    resp = make_response(
+        render_template(
+            fileName, X=X, Y=Y, graphImage=getGraphSet(graphId)["graphImage"]
+        )
+    )
     return resp
 
 
 def write_to_csv(dict):
-    field_names = ["time", "score"]
+    field_names = ["graphId", "time", "score"]
 
     with open("data.csv", "a") as csv_file:
         dict_object = csv.DictWriter(csv_file, fieldnames=field_names)
@@ -112,16 +133,15 @@ def write_to_csv(dict):
         dict_object.writerow(dict)
 
 
-def read_csv(path):
+def read_csv(path, graphId):
     x = []
     y = []
     with open(path, "r") as file:
         reader = csv.reader(file)
         for row in reader:
-            print(row)
             if row:
-                x.append(float(row[0]))
-                y.append(float(row[1]))
+                x.append(float(row[1]))
+                y.append(float(row[2]))
     return (x, y)
 
 
